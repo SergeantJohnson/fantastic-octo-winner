f34ca298297d0403639fd3c694cc61ede43582a7
SergeantJohnson <72981182+SergeantJohnson@users.noreply.github.com>
2021-05-06 12:30:07 +0530

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-
-var food=[];
-function get_menu_list() {
-    
-    food.push(document.getElementById("foodz").value);
-    food.sort();
-    document.getElementById("menu_list").innerHTML=food;
-    document.getElementById("foodz").value="";
-}
